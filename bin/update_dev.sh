1b54912e45e286ba127a2f6228e416c5a19d4d10
MD Mehedi Hasan <mehidy.gb@gmail.com>
2024-04-25 20:41:53 +0600

    modify commands

--- bin/update_dev.sh
+++ bin/update_dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 composer install
-php artisan config:clear
-php artisan migrate --seed --force
 php artisan optimize:clear
+php artisan migrate --seed --force
 php artisan optimize
 chown -R www-data:www-data /app
