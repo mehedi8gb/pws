1e3350c9e166db440f060bef0639fc3f407d9f83
said-dnp <127597282+said-dnp@users.noreply.github.com>
2024-05-01 20:38:32 +0100

    Remove /phpinfo and /debug-sentry test routes (#13)
    Co-authored-by: Said <said@Saids-Mac-mini.local>

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,3 @@ use Illuminate\Support\Facades\App;
 Route::get('/', function () {
     return view('welcome');
 });
-
-//   temporary route for testing
-Route::get('/phpinfo', function (){
-    return phpinfo();
-
-});
-
-// sentry test route
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
